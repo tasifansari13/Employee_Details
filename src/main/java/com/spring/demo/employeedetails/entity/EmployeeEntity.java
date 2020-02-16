eeb810a69997c4b4b74624f0aa0b3e1efd9039c8
Tasif <rohan@innocric.com>
2020-02-16 23:08:45 +0530

    Initial commit

---
+++ src/main/java/com/spring/demo/employeedetails/entity/EmployeeEntity.java
@@ -0,0 +1,33 @@
+package com.spring.demo.employeedetails.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EMPLOYEES")
+public class EmployeeEntity {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email", nullable = false, length = 200)
+	private String email;
+
+	// Setters and getters
+
+	@Override
+	public String toString() {
+		return "EmployeeEntity [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ "]";
+	}
+}
